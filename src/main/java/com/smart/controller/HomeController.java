cbae7af893e4597c9cc761a47805354ccdb5bab1
Rinkesh Goyal <rinkeshgoyal95@gmail.com>
2021-06-13 19:16:27 +0530

    Changed HomeController

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -11,20 +11,5 @@ import com.smart.entities.User;
 @Controller
 public class HomeController {
 	
-	@Autowired
 	
-	private UserRepository userRepo;
-	
-	@GetMapping("/test")
-	@ResponseBody
-	
-	
-	public String test() {
-		User user = new User();
-		
-		user.setName("Rinkesh");
-		user.setEmail("rinkeshgoyal95@gmail.com");
-		userRepo.save(user);
-		return "Working";
-	}
 }
